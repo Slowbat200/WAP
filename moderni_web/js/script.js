fdeac2b48116181e9aa39c320d7437ab3f852c2d
Jakub Pavlovič <92738343+Slowbat200@users.noreply.github.com>
2022-02-22 19:39:38 +0100

    Update script.js

--- moderni_web/js/script.js
+++ moderni_web/js/script.js
@@ -1,1 +1,1 @@
-alert("Na této stránce jsou pojmy, kterým byste neuseli rozumnět. Na konci stránky je slovník pojmů, který vám umožní pochopit lépe pojmy")
+alert("Na této stránce jsou pojmy, kterým byste nemuseli rozumnět. Na konci stránky je slovník pojmů, který vám umožní pochopit lépe pojmy")
